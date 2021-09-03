07dab6e9f0527a6917d79ac989b14f93e62c7dee
cmaldonado98 <carlos.maldonado1@cloud.uautonoma.cl>
2021-09-03 09:36:01 -0500

    Identity en entity class y cambio properties

--- src/main/java/com/servicio/registrocliente/entity/Cliente.java
+++ src/main/java/com/servicio/registrocliente/entity/Cliente.java
@@ -16,7 +16,7 @@ import javax.persistence.Id;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long clienteId;
     private String nombreCliente;
     private String cedula;
